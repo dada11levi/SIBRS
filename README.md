8c9f21e1f2ed8cb7c6b6047e71af139b6831f8c0
GoofyAhhDaLe <dada11levi@gmail.com>
2024-06-11 14:16:04 +0200

    1.1

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ SIBRS is a simple batch file that plays mp3 files by random cooldowns. There are
 This project is inspired by silence interuppted by sound videos.
 
 To download properly click on the green code button and click on Download ZIP
- 
+
+Changelog:
+1.0: -
+1.1: -min cooldown set to 1s, max cooldown changes depending on the amount of sounds in the folder (base cooldown changed to 1800s).
--- SIBRS.bat
+++ SIBRS.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal enableDelayedExpansion
-set /a min = 300
-set /a max = 1200
 cd /d "%~dp0"
+echo You can minimize this window and Groove Music. To quit press CTRL+C or close the window.
+:loop
 set /a "i=0"
 for %%f in (*.mp3) do (
     set /a "i+=1"
     set "list[!i!]=%%~f"
 )
-echo You can minimize this window. To quit press CTRL+C or close the window.
-:loop
+set /a min = 1
+set /a max = 1800 / %i%
 set /a rndNum=( %RANDOM% %% (%max% - %min% + 1) ) + %min%
-timeout /t %rndNum% /nobreak >nul
+timeout /t %rndNum% 
 set /a rnd=%random% %%%i% +1
 start "" "!list[%rnd%]!"
 goto loop
